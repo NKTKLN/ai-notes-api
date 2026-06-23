cb14f210571b43a28cdbeba1c853b2cd5c952d39
NKTKLN <nktkln@nktkln.com>
2026-06-23 06:27:54 +0300

    feat(repository): update init file

--- src/ai_notes_api/repositories/__init__.py
+++ src/ai_notes_api/repositories/__init__.py
@@ -6,6 +6,8 @@ This package re-exports repository classes used for database access.
 from .base import BaseRepository
 from .chat_memory import ChatMemoryRepository
 from .chat_session import ChatSessionRepository
+from .document import DocumentRepository
+from .document_chunk import DocumentChunkRepository
 from .filters import (
     ChatSessionListFilters,
     GenerationJobListFilters,
@@ -15,6 +17,8 @@ from .filters import (
 from .generation_job import GenerationJobRepository
 from .message import MessageRepository
 from .note import NoteRepository
+from .rag_query import RagQueryRepository
+from .rag_query_source import RagQuerySourceRepository
 from .user import UserRepository
 
 __all__ = [
@@ -29,4 +33,8 @@ __all__ = [
     "NoteRepository",
     "UserRepository",
     "ChatMemoryRepository",
+    "DocumentRepository",
+    "DocumentChunkRepository",
+    "RagQueryRepository",
+    "RagQuerySourceRepository",
 ]
